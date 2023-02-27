0452b09eb23742c0011bc425be280abeed36a067
Mark Vivian <meliodasdragonsin008@gmail.com>
2023-02-27 22:52:06 +0300

    made a json file for the females and the users made a get and post api tested the api using postman

--- BackendServerNode/index.js
+++ BackendServerNode/index.js
@@ -1,22 +1,33 @@
 const Express = require("express")
 const Cors = require("cors")
-
+const Users = require("./Data/Users.json")
+const Females = require("./Data/Females.json")
 const App = Express()
 
 App.use(Cors())
+App.use(Express.json())
 
-App.get('/', (req, res) => {
-    res.send("hello wolrd")
+App.get('/getFemales', (req, res) => {
+    res.status(200).send(Females)
 })
 
-App.post("/", (req, res)=>{
+App.post("/postFemales", (req, res)=>{
     const Data = req.body
-    res.send(JSON.stringify({message : "the data has been recieved"}))
-
+    res.send(JSON.stringify({message : "the Females has been recieved"}))
 })
 
-App.listen(3000, ()=>{
-    console.log("the port is running")
+App.get('/getUsers', (req, res) => {
+    res.send(Users)
+})
+
+App.post("/postUsers", (req, res)=>{
+    const Data = req.body
+    res.send(JSON.stringify({message : "the Users has been recieved"}))
+})
+
+const port = 3001
+App.listen(port, ()=>{
+    console.log(`the port is running at ${port}`)
 })
 
 //to run the server we use node filename.js
--- website-next/Components/MainLayout.js
+++ website-next/Components/MainLayout.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Navbar from './Navbar/Navbar'
 import Footer from './Footer/Footer'
+import Header from './Header/Header'
 
 function MainLayout({children}) {
   return (
     <>
+        <Header />
+    
         <Navbar />
 
             {children}
